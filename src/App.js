38f8e6ec6016bc8fd8ffd44dfa60c5f6822f84ea
Kizito Azubuike <kizito.azubuike1@gmail.com>
2023-01-10 00:39:28 +0100

    setup project structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.scss'
+import Home from './views/Home'
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello world</h1>
+      <Home />
     </div>
   )
 }
---
+++ src/assets/images.js
@@ -0,0 +1,54 @@
+import bed from './images/Bed.png'
+import cleaning from './images/cleaning.png'
+import community from './images/community.png'
+import environment from './images/environment.png'
+import flexible from './images/flexible.png'
+import happiness from './images/happiness.png'
+import Hero from './images/Hero.jpg'
+import InfoSection from './images/InfoSection.png'
+import Landlord from './images/Landlord.png'
+import money from './images/money.png'
+import pay from './images/pay.png'
+import Property1 from './images/Property1.png'
+import Property2 from './images/Property2.png'
+import Property3 from './images/Property3.png'
+import Property4 from './images/Property4.png'
+import Property5 from './images/Property5.png'
+import Property6 from './images/Property6.png'
+import Roomate from './images/Roomate.png'
+import safe from './images/safe.png'
+import Shower from './images/Shower.png'
+import Size from './images/Size.png'
+import testimonial from './images/testimonial.png'
+import User from './images/User.png'
+import logo from './images/logo.png'
+
+export const images = {
+  Hero,
+  cleaning,
+  flexible,
+  happiness,
+  InfoSection,
+  Property1,
+  Property2,
+  Property3,
+  Property4,
+  Property5,
+  Property6,
+  Roomate,
+  testimonial,
+}
+
+export const icons = {
+  bed,
+  User,
+  Size,
+  Shower,
+  safe,
+  pay,
+  money,
+  Landlord,
+  environment,
+  community,
+  logo,
+}
